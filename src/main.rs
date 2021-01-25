bc45df65364de7ff62d393e8e7e81d2a49c76621
AMan <AMan@runningcoders.org>
2021-01-26 02:12:55 +0800

    feat(main): just test :beers:

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,21 @@ fn main() -> Result<(), io::Error> {
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     terminal.draw(|f| {
-        let size = f.size();
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10),
+                ]
+                .as_ref(),
+            )
+            .split(f.size());
         let block = Block::default().title("Block").borders(Borders::ALL);
-        f.render_widget(block, size);
+        f.render_widget(block, chunks[0]);
+        let block = Block::default().title("Block 2").borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
     })
 }
